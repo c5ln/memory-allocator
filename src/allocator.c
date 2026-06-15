5008a94dcce94714e4b5ab5f9bca95b6984889ab
c5ln <cjstjdgh0209@gmail.com>
2026-06-15 23:11:18 +0900

    refacto: my_malloc 코드 리팩토링 (비트 연산자 활용)

--- src/allocator.c
+++ src/allocator.c
@@ -4,20 +4,13 @@ static void *free_head = NULL; // free list 머리
 
 void *my_malloc(size_t size){
     if(size==0) return NULL;
-    size_t aligend = size; 
-
-    if (aligend % 16 < 12){
-        aligend = aligend + (12 - aligend%16);
-    }
-    else if(aligend % 16 > 12){
-        aligend = aligend + (16 - aligend%16) + 12;
-    }
+    size_t need = (size + 4 + 15) & ~(size_t)15;  // 16의 배수로 올림 연산. 이진수의 관점으로 보면 된다.
     void **link = &free_head;
     while(*link)
     {
         uint32_t* h = (uint32_t*)(*link);
         size_t chunk_size = *h & ~15u;
-        if(chunk_size >= aligend + 4){
+        if(chunk_size >= need){
             *h = *h | 1;
             void *payload = (char*)(*link)+4;
             *link = *(void**)payload;
@@ -29,13 +22,13 @@ void *my_malloc(size_t size){
     uintptr_t pad = ((uintptr_t)12 - cur) & 15; // 하위 4비트만 가져오기
 
      // flag 추가. LSB가 1이면 사용중
-    void *p = sbrk(aligend+pad+4);
+    void *p = sbrk(need+pad);
     if(p==(void*)(-1)){
         return NULL;
     }
     char *header = (char*)p + pad;
     char *payload = header+4;
-    *(uint32_t*)header = (uint32_t)((aligend+4)) | 1 ;
+    *(uint32_t*)header = (uint32_t)need | 1 ;
 
     return (void*)payload;
 }
